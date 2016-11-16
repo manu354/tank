b5cd4342e22672ae48091bdd758507a295647e51
Manu Masson <manu.masson@RH-A13-16-21.local>
2016-11-17 09:53:42 +1100

    Arduino

---
+++ arduino/arrows.ino
@@ -0,0 +1,58 @@
+/*
+  Esplora Rotate Orientation
+  
+  Author: Manu Masson
+  School: Reddamhouse
+  Date: DATE
+  Description: Detects the orientation of the Esplora board and,
+  changes the LED colour according to the following orientations:
+    Top Upright= Blue
+    Left Upright = Red
+    Right Upright = Green
+    Bottom Upright = Yellow
+  
+  The Keyboard Modifiers are: KEY_LEFT_ARROW, KEY_RIGHT_ARROW, KEY_UP_ARROW, KEY_DOWN_ARROW
+  
+  For example Keyboard.press(KEY_LEFT_ARROW); will press the left arrow.
+ */
+
+#include <Esplora.h>
+#include <Keyboard.h>
+
+void setup() {
+  Keyboard.begin();
+} 
+
+void loop()
+{
+  int xAxis = Esplora.readAccelerometer(X_AXIS);    // read the X axis
+  int yAxis = Esplora.readAccelerometer(Y_AXIS);    // read the Y axis
+  int zAxis = Esplora.readAccelerometer(Z_AXIS);    // read the Z axis
+  
+  if(yAxis > 150 && yAxis > xAxis && yAxis > zAxis){          
+    Esplora.writeRGB(0,0,200);
+    Keyboard.press(KEY_UP_ARROW);
+  }
+  else if (xAxis < -100 && abs(xAxis) > zAxis && abs(xAxis) > yAxis){
+   Esplora.writeRGB(200,0,0);
+  Keyboard.press(KEY_LEFT_ARROW);
+  }
+  
+  else if (yAxis < -100 && abs(yAxis) > xAxis && abs(yAxis) > zAxis){
+    Esplora.writeRGB(200,200, 0);
+    Keyboard.press(KEY_DOWN_ARROW);
+  }
+  else if (xAxis > 100 && abs(xAxis) > zAxis && abs(xAxis) > yAxis){
+    Esplora.writeRGB(0,200,0);
+    Keyboard.press(KEY_RIGHT_ARROW);
+  }
+
+  else if (Esplora.readButton(1) == LOW){
+     Esplora.tone(100, 500) ;
+    Keyboard.press('/');
+  }
+  
+  else{
+  Keyboard.releaseAll();      // release all the keys
+  }
+}
